99944d8726dcb78996b5e1a35ed9a222225ac09f
Jesús Valladolid Rebollar <chus@chusito.es>
2017-12-11 15:39:23 +0100

    buernoooo

--- LosBarkitosIpad/marinaFerryViewController.swift
+++ LosBarkitosIpad/marinaFerryViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 class marinaferryViewController: UIViewController {
-
+    
+    @IBOutlet weak var partGrupView: UIView!
+    @IBOutlet weak var partButton: UIButton!
+    @IBOutlet weak var gruposButton: UIButton!
+    @IBOutlet weak var precioPartView: UIView!
+    @IBOutlet weak var precioGruposView: UIView!
+    
+    @IBAction func partPush(_ sender: UIButton) {
+        self.precioGruposView.alpha = 1
+        self.precioPartView.alpha = 0
+    }
+    @IBAction func gruposPush(_ sender: UIButton) {
+        self.precioGruposView.alpha = 0
+        self.precioPartView.alpha = 1
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let alertaNOInternet = UIAlertController(title: "SIN CONEXIÓN!!!", message: "No hay conexión y no se ha incluido el ticket en el listado. Llama al Chus lo antes posible!!!", preferredStyle: UIAlertControllerStyle.alert)
@@ -19,24 +35,27 @@ class marinaferryViewController: UIViewController {
         alertaNOInternet.addAction(OkAction)
         
         self.present(alertaNOInternet, animated: true, completion: nil)
-
+        
         // Do any additional setup after loading the view.
+        self.precioPartView.alpha = 0
+        self.precioGruposView.alpha = 0
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
